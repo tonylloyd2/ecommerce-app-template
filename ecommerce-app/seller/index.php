754438c2ca6a26871c7342376c43a957fc67d169
tonylloyd2 <lloydkatila@gmail.com>
2023-08-15 04:12:37 +0300

    seller

--- ecommerce-app/seller/index.php
+++ ecommerce-app/seller/index.php
@@ -1,172 +1,147 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html class="no-js" lang="en">
+    <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="description" content=" " />
         <!-- Title -->
-        <title>Cart Style2 - Posh Auto Parts eCommerce Bootstrap 4 Html Template</title>
+        <title>Register - E-Shopper-Goodies</title>
         <!-- Favicon  -->
         <link rel="shortcut icon" href="assets/images/fevicon.png" />
 
         <!-- *********** CSS Files *********** -->
         <!-- Plugin CSS -->
-        <link rel="stylesheet" href="assets/css/plugins.css" />
+        <link rel="stylesheet" href="../assets/css/plugins.css" />
         <!-- Styles CSS -->
-        <link rel="stylesheet" href="assets/css/styles.css" />
-        <link rel="stylesheet" href="assets/css/responsive.css" />    
-        <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-   
+        <link rel="stylesheet" href="../assets/css/styles.css" />
+        <link rel="stylesheet" href="../assets/css/responsive.css" />       
     </head>
-<body>
-<style>
-  .form-group {
-    display: inline-block;
-    width: 33.33%;
-  }
-</style>
 
-<form>
-  <div class="form-group">
-    <label for="item_name">Item Name:</label>
-    <input type="text" class="form-control" id="item_name" name="item_name" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="category">Category:</label>
-    <input type="text" class="form-control" id="category" name="category" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="items_number">Items Number:</label>
-    <input type="text" class="form-control" id="items_number" name="items_number" value="5">
-  </div>
-  
-  <div class="form-group">
-    <label for="image_primary">Primary Image:</label>
-    <input type="text" class="form-control" id="image_primary" name="image_primary" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="image2">Image 2:</label>
-    <input type="text" class="form-control" id="image2" name="image2" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="image3">Image 3:</label>
-    <input type="text" class="form-control" id="image3" name="image3" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="image4">Image 4:</label>
-    <input type="text" class="form-control" id="image4" name="image4" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="image5">Image 5:</label>
-    <input type="text" class="form-control" id="image5" name="image5" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="image6">Image 6:</label>
-    <input type="text" class="form-control" id="image6" name="image6" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="description">Description:</label>
-    <textarea class="form-control" id="description" name="description" required></textarea>
-  </div>
-  
-  <div class="form-group">
-    <label for="price">Price:</label>
-    <input type="text" class="form-control" id="price" name="price" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="wishlist_tag">Wishlist Tag:</label>
-    <input type="checkbox" id="wishlist_tag" name="wishlist_tag" value="true">
-  </div>
-  
-  <div class="form-group">
-    <label for="cart_tag">Cart Tag:</label>
-    <input type="checkbox" id="cart_tag" name="cart_tag" value="true">
-  </div>
-  
-  <div class="form-group">
-    <label for="new_arrival_tag">New Arrival Tag:</label>
-    <input type="checkbox" id="new_arrival_tag" name="new_arrival_tag" value="true">
-  </div>
-  
-  <div class="form-group">
-    <label for="in_demand_tag">In Demand Tag:</label>
-    <input type="checkbox" id="in_demand_tag" name="in_demand_tag" value="true">
-  </div>
-  
-  <div class="form-group">
-    <label for="trending_tag">Trending Tag:</label>
-    <input type="checkbox" id="trending_tag" name="trending_tag" value="true">
-  </div>
-  
-  <div class="form-group">
-    <label for="flash_sale_tag">Flash Sale Tag:</label>
-    <input type="checkbox" id="flash_sale_tag" name="flash_sale_tag" value="true">
-  </div>
-  
-  <div class="form-group">
-    <label for="availability">Availability:</label>
-    <select class="form-control" id="availability" name="availability">
-      <option value="true">Available</option>
-      <option value="false">Not Available</option>
-    </select>
-  </div>
-  
-  <div class="form-group">
-    <label for="ratings">Ratings:</label>
-    <input type="text" class="form-control" id="ratings" name="ratings">
-  </div>
-  
-  <div class="form-group">
-    <label for="reviews">Reviews:</label>
-    <input type="text" class="form-control" id="reviews" name="reviews">
-  </div>
-  
-  <div class="form-group">
-    <label for="seller_name">Seller Name:</label>
-    <input type="text" class="form-control" id="seller_name" name="seller_name" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="seller_phone_contact">Seller Phone Contact:</label>
-    <input type="text" class="form-control" id="seller_phone_contact" name="seller_phone_contact" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="seller_email_contact">Seller Email Contact:</label>
-    <input type="email" class="form-control" id="seller_email_contact" name="seller_email_contact" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="seller_website_link">Seller Website Link:</label>
-    <input type="text" class="form-control" id="seller_website_link" name="seller_website_link" required>
-  </div>
-  
-  <div class="form-group">
-    <label for="seller_organization">Seller Organization:</label>
-    <input type="text" class="form-control" id="seller_organization" name="seller_organization" value="Self Business">
-  </div>
-  
-  <div class="form-group">
-    <label for="coming_soon">Coming Soon:</label>
-    <input type="checkbox" id="coming_soon" name="coming_soon" value="true">
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</body>
+    <body class="template-register account-page">
+        <!-- Start Page Loader -->
+        <div class="page-loading"></div>
+        <!-- End Page Loader -->
+
+        <!--  Start Main Wrapper -->
+        <div class="main-wrapper cart-drawer-push">
+            <!-- Start Promotional Bar Section -->
+            
+            <!-- End Promotional Bar Section -->
+
+            <!-- Start Header Section -->
+            
+            <!-- End Header Section -->
+
+            <!-- Start Main Content -->
+            <main class="main-content">
+                <!-- Start Breadcrumb -->
+                <div class="breadcrumbs text-center">
+                    <div class="container">
+                        <h1>Create An Account</h1>
+                        <ul class="breadcrumb bg-transparent m-0 p-0 justify-content-center">
+                            <li class="breadcrumb-item"><a href="index.php" title="Home">Home</a></li>
+                            <li class="breadcrumb-item active">Create an Account</li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- End Breadcrumb -->
+
+                <!-- Start Register Account -->
+                <div class="create-account">
+                    <div class="container">
+                        <div class="row row-sp">
+                            <div class="col-sp col-12 col-sm-12 col-md-12 col-lg-12">
+                                <div class="page-title text-center">
+                                    <p class="subtitle mb-0">
+                                        Create your Account as a seller and upload your products
+                                    </p>
+                                </div>
+                                <form action="../backend/seller/auth/register.php" class="register-form needs-validation" enctype="multipart/form-data" method="post" >
+                                    <div class="row">
+                                        <div class="form-group col-md-6">
+                                            <label>First Name *</label>
+                                            <input type="text" class="form-control" placeholder="" required name="first-name" />
+                                            <div class="invalid-feedback">Please enter your first name.</div>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label>Last Name *</label>
+                                            <input type="text" class="form-control" placeholder="" required name="last-name" />
+                                            <div class="invalid-feedback">Please enter your last name.</div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-md-6">
+                                            <label>Organization Name*</label>
+                                            <input type="email" class="form-control" placeholder="" required  name="company"  />
+                                            <div class="invalid-feedback">Please enter your bussiness name.</div>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label>Bussiness Account number *</label>
+                                            <input type="number" class="form-control" placeholder="" required  name="bussiness-account-number" />
+                                            <div class="invalid-feedback">Please enter your bussiness account number.</div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-md-6">
+                                            <label>Email Address *</label>
+                                            <input type="email" class="form-control" placeholder="" required  name="email"  />
+                                            <div class="invalid-feedback">Please enter your email.</div>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label>Phone number *</label>
+                                            <input type="number" class="form-control" placeholder="" required  name="phone" />
+                                            <div class="invalid-feedback">Please enter your phone number.</div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                    <div class="form-group col-md-4">
+                                            <label>Profile Picture / Bussiness logo *</label>
+                                            <input  type="file" name="filetoupload"  required/>
+                                            <div class="invalid-feedback">Please enter your Profile Picture / Bussiness logo</div>
+                                        </div>
+                                        <div class="form-group col-md-8">
+                                            <label>Password *</label>
+                                            <input type="password" class="form-control" placeholder="" required  name="password"  required/>
+                                            <div class="invalid-feedback">Please enter your password.</div>
+                                        </div>                                       
+                                    </div>
+                                    
+                                    
+                                    <div class="form-group button-action mt-1 mt-sm-5 clearfix text-center">
+                                        <button type="submit" class="account-create btn btn-primary" name="submit">Create Seller Account</button>
+                                        <p><a href="./login.php"></a></p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Register Account -->
+            </main>
+            <!-- End Main Content -->
+
+            <!-- Start Footer Section -->
+            <!-- End Footer Section -->
+
+            <!-- Start Scroll Top -->
+            <div id="scrollTop"><i class="ti-arrow-up"></i></div>
+            <!-- End Scroll Top -->
+
+            <!-- Overlay -->
+            <div class="overlay"></div>
+
+        </div>
+        <!--  End Main Wrapper -->
+
+        <!-- ******** JS Files ******** -->        
+        <!-- Plugin JS -->	        
+        <script src="../assets/js/plugins.js"></script>
+
+        <!-- Main JS -->
+        <script src="../assets/js/main.js"></script>
+
+    </body>
 </html>
+
