a4abaa0b25baa83b820dc8d175f862466d57b687
tonylloyd2 <lloydkatila@gmail.com>
2023-08-16 02:08:41 +0300

    selleeer

--- ecommerce-app/backend/seller/auth/register.php
+++ ecommerce-app/backend/seller/auth/register.php
@@ -28,17 +28,6 @@ $target_file = $target_dir . basename($_FILES["filetoupload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["image"]["tmp_name"]);
-//   if($check !== false) {
-//     echo "File is an image - " . $check["mime"] . ".";
-//     $uploadOk = 1;
-//   } else {
-//     echo "File is not an image.";
-//     $uploadOk = 0;
-//   }
-// }
 
 // Check if file already exists
 if (file_exists($target_file)) {
